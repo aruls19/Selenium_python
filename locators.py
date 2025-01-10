197458e7b5f29b08573bb7995b9300e82bdb2adb
aruls19 <arulstiger@gmail.com>
2025-01-11 00:23:51 +0530

    Create locators.py

---
+++ locators.py
@@ -0,0 +1,25 @@
+lead_pagetitle="//div/span[text()='Leads']"
+
+lead_new_button="//div[@title='New']"
+new_lead_window="//div/h2[text()='New Lead']"
+lead_firstname="//input[@name='firstName']"
+lead_lastname="//input[@name='lastName']"
+lead_companyname="//input[@name='Company']"
+lead_savebutton="//button[text()='Save']"
+
+leadpage_convertbutton="//button[@name='Convert']"
+lead_convertwindow="//h1[contains(text(),'Convert Lead ')]"
+leadtoaccount_convert_button="(//div/span/button[@type='button'])[2]"
+
+account_name_link="//div[@class='primaryField truncate']/a[@title='OLD']"
+account_pagetitle="//slot[@name='entityLabel']/records-entity-label[contains(text(),'Account')]"
+
+new_contact_button="//button[@name='Global.NewContact']"
+new_contact_window="//h2[contains(text(),'New Contact')]"
+contact_firstname="//input[@placeholder='First Name']"
+contact_lastname="//input[@class='lastName compoundBLRadius compoundBRRadius form-element__row input']"
+contact_savebutton="(//span[contains(text(),'Save') and @class=' label bBody'])[4]"
+
+new_opportunity_button="//button[@name='Global.NewOpportunity']"
+new_opportunity_window="//h2[contains(text(),'New Opportunity')]"
+new_opportunity_savebutton="(//span[@class = ' label bBody'])[7]"
